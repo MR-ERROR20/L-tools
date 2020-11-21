fedf994a9af3469deeea5140ca7cd77538081a5b
LBOT GANSS <73551794+MR-ERROR20@users.noreply.github.com>
2020-11-21 11:46:21 +0700

    Add files via upload

---
+++ chrome.py
@@ -0,0 +1,67 @@
+import os, sys
+
+print ("\033[1;32mSilahkan Masukkan Username & Password Anda")
+
+print ("\033[1;32matau silahkan Hubungi MRR ")
+
+username = 'Lucky'      
+
+password = 'gans'
+
+
+
+def restart():
+
+	ngulang = sys.executable
+
+	os.execl(ngulang, ngulang, *sys.argv)
+
+
+
+def main():
+
+	uname = raw_input("username : ")
+
+	if uname == username:
+
+		pwd = raw_input("password : ")
+
+
+
+		if pwd == password:
+
+			print "\033[1;32mAlhmdllh sudah masuk juga..", 
+
+			sys.exit()
+
+
+
+		else:
+
+			print "\033[1;32mMaaf password salah,nulis PW yg bener ajg... [?]\033[00m"
+
+			print "Silahkan segera log-in kembali...!!\n"
+
+			restart()
+
+
+
+	else:
+
+		print "\033[1;32mMaaf Username salah,yg bener ajg... [?]\033[00m"
+
+		print "Silahkan segera log-in kembali...!!\n"
+
+		restart()
+
+
+
+try:
+
+	main()
+
+except KeyboardInterrupt:
+
+	os.system('clear')
+
+	restart()
